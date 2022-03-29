34dee7cbce94ad677a6df33686544960d9dd3517
Ranieri Althoff <ranieri.althoff@klarna.com>
2022-03-29 14:14:22 +0200

    Remove scripts from output and fix above-the-fold image loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,24 @@ module.exports = {
     title: 'Gatsby + Node.js (TypeScript) API',
   },
   plugins: [
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Gatsby + Node.js (TypeScript) API',
         short_name: 'Gatsby + Node.js (TypeScript)',
         start_url: '/',
         icon: 'src/images/gatsby-icon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-no-javascript-utils',
+      options: {
+	noScript: true,
+	removeReactHelmetAttrs: true,
+	noInlineStyles: true,
+	removeGatsbyAnnouncer: true,
       },
     },
   ],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -105,7 +105,7 @@ const Index = () => (
     </Helmet>
 
     <Hero>
-      <img src={meta.heroImage} height={300} width={300} loading="lazy" />
+      <img src={meta.heroImage} height={300} width={300} />
 
       <h1>{meta.heroTitle}</h1>
       <p>
